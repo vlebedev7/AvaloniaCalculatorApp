37c7d89f9436545bef067abb958f8895a5ad36df
Vitaliy Lebedev <vitaliy.lebedev7@gmail.com>
2023-03-04 05:04:06 +0600

    add calculation result

--- AvaloniaTestApp1/MainWindow.axaml.cs
+++ AvaloniaTestApp1/MainWindow.axaml.cs
@@ -4,7 +4,6 @@ using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Microsoft.CodeAnalysis;
 using System.Reflection.PortableExecutable;
-using System.Data.SqlTypes;
 
 namespace AvaloniaTestApp1
 {
@@ -13,12 +12,23 @@ namespace AvaloniaTestApp1
         public MainWindow()
         {
             InitializeComponent();
+            ResetValues();
         }
 
-        public bool IsFirstNumber { get; set; } = true;
+        public bool IsFirstNumber { get; set; }
         public int FirstNumber { get; set; }
         public int SecondNumber { get; set; }
+        public int CurrentNumber => IsFirstNumber ? FirstNumber : SecondNumber;
         public char Sign { get; set; }
+        public string Result { get; set; }
+
+        private void ResetValues()
+        {
+            FirstNumber = 0;
+            SecondNumber = 0;
+            IsFirstNumber = true;
+            Sign = (char)0;
+        }
 
         private int ApplyDigit(int number, int digit) => 
             number * 10 + digit;
@@ -41,9 +51,43 @@ namespace AvaloniaTestApp1
             UpdateDataContext();
         }
 
+        public void OnClickEquals(object sender, RoutedEventArgs e)
+        {
+            if (Sign == 0)
+                return;
+            Result = Sign switch
+            {
+                '+' => (FirstNumber + SecondNumber).ToString(),
+                '-' => (FirstNumber - SecondNumber).ToString(),
+                '*' => (1.0f * FirstNumber * SecondNumber).ToString(),
+                '/' => SecondNumber == 0
+                    ? "Division by zero error"
+                    : (1.0f * FirstNumber / SecondNumber).ToString(),
+                _ => "No sign",
+            };
+
+            ResetValues();
+            UpdateDataContext();
+        }
+
+
         private void UpdateDataContext()
         {
-            (DataContext as MainViewModel).MainTextBox = FirstNumber.ToString();
+            string numberStr = "";
+            if (string.IsNullOrWhiteSpace(Result)) // show input values
+            {
+                if (FirstNumber != 0)
+                    numberStr += FirstNumber;
+                if (Sign != 0)
+                    numberStr += Sign;
+                if (Sign != 0 && SecondNumber != 0)
+                    numberStr += SecondNumber;
+            }
+            else // show result
+            {
+                numberStr = Result;
+            }
+            (DataContext as MainViewModel).MainTextBox = numberStr;
         }
     }
 }

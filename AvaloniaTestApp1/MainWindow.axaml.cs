f06a3a34a3f473042eb448c8b27e963311b6e375
Vitaliy Lebedev <vitaliy.lebedev7@gmail.com>
2023-03-04 01:56:25 +0600

    create new solution

---
+++ AvaloniaTestApp1/MainWindow.axaml.cs
@@ -0,0 +1,14 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Microsoft.CodeAnalysis;
+
+namespace AvaloniaTestApp1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}

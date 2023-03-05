b2c36df8da74827137f3cbc9845ee85596794f50
Vitaliy Lebedev <vitaliy.lebedev7@gmail.com>
2023-03-05 22:49:36 +0600

    implement decimal numbers calculation

--- AvaloniaTestApp1/MainWindow.axaml.cs
+++ AvaloniaTestApp1/MainWindow.axaml.cs
@@ -2,58 +2,90 @@ using System;
 using System.Linq;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Avalonia.VisualTree;
 using Microsoft.CodeAnalysis;
 using System.Reflection.PortableExecutable;
+using System.Reactive.Disposables;
 
 namespace AvaloniaTestApp1
 {
     public partial class MainWindow : Window
     {
+        public bool IsFirstNumber { get; set; }
+        public string FirstNumber { get; set; }
+        public string SecondNumber { get; set; }
+        public string CurrentNumber => IsFirstNumber ? FirstNumber : SecondNumber;
+        public char Sign { get; set; }
+        public string Result { get; set; }
+
         public MainWindow()
         {
+            this.Activated += (object sender, EventArgs wat) =>
+            {
+                var els = this.GetVisualDescendants().OfType<Button>();
+                foreach (var el in this.GetVisualDescendants().OfType<Button>())
+                {
+                    //el.Width = 30;
+                }
+            };
             InitializeComponent();
             ResetValues();
         }
 
-        public bool IsFirstNumber { get; set; }
-        public int FirstNumber { get; set; }
-        public int SecondNumber { get; set; }
-        public int CurrentNumber => IsFirstNumber ? FirstNumber : SecondNumber;
-        public char Sign { get; set; }
-        public string Result { get; set; }
-
         private void ResetValues()
         {
-            FirstNumber = 0;
-            SecondNumber = 0;
+            FirstNumber = "";
+            SecondNumber = "";
             IsFirstNumber = true;
             Sign = (char)0;
         }
-
-        private int ApplyDigit(int number, int digit) => 
-            number * 10 + digit;
 
         public void OnClickDigit(object sender, RoutedEventArgs e)
         {
             if (!string.IsNullOrWhiteSpace(Result))
                 Result = null; // start new calculation
             int digit = int.Parse((sender as Button).Content as string);
+            if (CurrentNumber == "" && digit == 0)
+                return; // no trailing zeroes
 
             if (IsFirstNumber)
-                FirstNumber = ApplyDigit(FirstNumber, digit);
+                FirstNumber += digit;
             else
-                SecondNumber = ApplyDigit(SecondNumber, digit);
+                SecondNumber += digit;
             UpdateDataContext();
         }
 
         public void OnClickSign(object sender, RoutedEventArgs e)
         {
             if (!string.IsNullOrWhiteSpace(Result))
-                Result = null; // start new calculation
-            if (FirstNumber == 0)
-                return;
+            {
+                bool isDecimal = decimal.TryParse(Result, out _);
+                if (isDecimal) // if result is a valid number
+                    FirstNumber = Result; // use result as first number in the new calculation
+                Result = null; // clear result field
+            }
+            if (string.IsNullOrWhiteSpace(FirstNumber))
+                return; // don't set sign if first number is not set
             Sign = ((sender as Button).Content as string).Trim()[0];
             IsFirstNumber = false;
+            UpdateDataContext();
+        }
+
+        public void OnClickDecimalPoint(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(Result))
+                Result = null; // start new calculation
+
+            string currentNumber = CurrentNumber;
+            if (currentNumber == "")
+                currentNumber = "0.";
+            else
+                currentNumber += ".";
+
+            if (IsFirstNumber)
+                FirstNumber = currentNumber;
+            else
+                SecondNumber = currentNumber;
             UpdateDataContext();
         }
 
@@ -61,14 +93,17 @@ namespace AvaloniaTestApp1
         {
             if (Sign == 0)
                 return;
+            decimal firstNumber = decimal.Parse(FirstNumber);
+            decimal secondNumber = decimal.Parse(SecondNumber);
+
             Result = Sign switch
             {
-                '+' => (FirstNumber + SecondNumber).ToString(),
-                '-' => (FirstNumber - SecondNumber).ToString(),
-                '*' => (1.0f * FirstNumber * SecondNumber).ToString(),
-                '/' => SecondNumber == 0
+                '+' => (firstNumber + secondNumber).ToString(),
+                '-' => (firstNumber - secondNumber).ToString(),
+                '*' => (firstNumber * secondNumber).ToString(),
+                '/' => secondNumber == 0
                     ? "Division by zero error"
-                    : (1.0f * FirstNumber / SecondNumber).ToString(),
+                    : (firstNumber / secondNumber).ToString(),
                 _ => "No sign",
             };
 
@@ -81,11 +116,10 @@ namespace AvaloniaTestApp1
             string numberStr = "";
             if (string.IsNullOrWhiteSpace(Result)) // show input values
             {
-                if (FirstNumber != 0)
-                    numberStr += FirstNumber;
+                numberStr += FirstNumber;
                 if (Sign != 0)
                     numberStr += Sign;
-                if (Sign != 0 && SecondNumber != 0)
+                if (Sign != 0 && SecondNumber != "")
                     numberStr += SecondNumber;
             }
             else // show result
